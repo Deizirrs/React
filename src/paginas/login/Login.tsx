3213580ddaee12e0850696a6d0c880ad375ab204
deizirrs <deizianesantos.rodrigues15@gmail.com>
2023-06-08 20:08:58 -0300

    Update Login

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useState, useEffect } from "react";
-import './Login.css';
+import "./Login.css";
 import { Box, Grid, Button, TextField, Typography } from "@material-ui/core";
 import { Link, useNavigate } from "react-router-dom";
 import useLocalStorage from "react-use-localstorage";
@@ -41,7 +41,14 @@ function Login() {
       [e.target.name]: e.target.value,
     });
   }
- 
+
+  // useEffect(() => {
+  //   if (token !== "") {
+  //     dispatch(addToken(token));
+  //     navigate("/home");
+  //   }
+  // }, [token]);
+
   useEffect(() => {
     if (respUserLogin.token !== '') {
       console.log(respUserLogin.token)
